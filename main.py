9c8d37098762f6b493d3e2f1f1be79600f43e09c
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-20 12:10:59 +0000

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+from coffee_machine import CoffeeMachine
+
+def main() -> None:
+    new_coffee_machine = CoffeeMachine()
+    new_coffee_machine.make_coffee()
+
+if __name__ == "__main__":
+    main()
